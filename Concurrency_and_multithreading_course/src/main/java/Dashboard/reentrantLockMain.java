34e6091c7572c54a5e65bfaec24a84bd2601c108
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-01-17 16:22:58 +0100

    create priceupdater thread

--- Concurrency_and_multithreading_course/src/main/java/Dashboard/reentrantLockMain.java
+++ Concurrency_and_multithreading_course/src/main/java/Dashboard/reentrantLockMain.java
@@ -1,5 +1,8 @@
 package Dashboard;
 
+import lombok.Data;
+
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,6 +10,7 @@ public class reentrantLockMain {
     public static void main(String... args){
 
     }
+    @Data
     public static class PricesContainer{
         private Lock lockObject = new ReentrantLock();
         private double bitcoinPrice;
@@ -15,5 +19,38 @@ public class reentrantLockMain {
         private double bitcoinCashPrice;
         private double ripplePrice;
 
+    }
+    private static class PriceUpdater extends Thread{
+        private PricesContainer pricesContainer;
+        private Random random = new Random();
+        public  PriceUpdater(PricesContainer pricesContainer){
+            this.pricesContainer = pricesContainer;
+        }
+        @Override
+        public void run(){
+            while (true){
+                pricesContainer.getLockObject().lock();
+                try {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    pricesContainer.setBitcoinPrice(random.nextDouble(2000));
+                    pricesContainer.setEtherPrice(random.nextDouble(2000));
+                    pricesContainer.setLitecoinPrice(random.nextDouble(500));
+                    pricesContainer.setBitcoinCashPrice(random.nextDouble(5000));
+                    pricesContainer.setRipplePrice(random.nextDouble());
+                }
+                finally {
+                    pricesContainer.getLockObject().unlock();
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
