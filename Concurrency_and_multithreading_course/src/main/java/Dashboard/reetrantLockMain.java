04dda561f70d0aada6abab825c2d2205ea1486bb
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-01-17 16:45:41 +0100

    added lombk dependency

--- Concurrency_and_multithreading_course/src/main/java/Dashboard/reetrantLockMain.java
+++ Concurrency_and_multithreading_course/src/main/java/Dashboard/reetrantLockMain.java
@@ -19,6 +19,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import lombok.Data;
 
 import java.util.*;
 import java.util.concurrent.locks.Lock;
@@ -163,6 +164,7 @@ public class reetrantLockMain extends Application {
         System.exit(0);
     }
 
+    @Data
     public static class PricesContainer {
         private Lock lockObject = new ReentrantLock();
 
@@ -172,49 +174,7 @@ public class reetrantLockMain extends Application {
         private double bitcoinCashPrice;
         private double ripplePrice;
 
-        public Lock getLockObject() {
-            return lockObject;
-        }
 
-        public double getBitcoinPrice() {
-            return bitcoinPrice;
-        }
-
-        public void setBitcoinPrice(double bitcoinPrice) {
-            this.bitcoinPrice = bitcoinPrice;
-        }
-
-        public double getEtherPrice() {
-            return etherPrice;
-        }
-
-        public void setEtherPrice(double etherPrice) {
-            this.etherPrice = etherPrice;
-        }
-
-        public double getLitecoinPrice() {
-            return litecoinPrice;
-        }
-
-        public void setLitecoinPrice(double litecoinPrice) {
-            this.litecoinPrice = litecoinPrice;
-        }
-
-        public double getBitcoinCashPrice() {
-            return bitcoinCashPrice;
-        }
-
-        public void setBitcoinCashPrice(double bitcoinCashPrice) {
-            this.bitcoinCashPrice = bitcoinCashPrice;
-        }
-
-        public double getRipplePrice() {
-            return ripplePrice;
-        }
-
-        public void setRipplePrice(double ripplePrice) {
-            this.ripplePrice = ripplePrice;
-        }
     }
 
     public static class PriceUpdater extends Thread {
